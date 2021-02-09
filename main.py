4f4507cd12add4a2faba1c1035f3f97297c5ce91
Xavier Nodet <xavier.nodet@fr.ibm.com>
2021-02-09 10:52:08 +0100

    Just a function that accepts a path
    And all the configuration for the project.

---
+++ main.py
@@ -0,0 +1,20 @@
+""""Send a CPLEX file to WML and solve it.
+
+For now, just accepts a path and sends that, assuming that's a model"""
+
+
+def solve(path):
+    """Solve the model.
+
+    The model is sent as online data to WML.
+
+    Args:
+        path: pathname to the file to solve"""
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    solve('acc-tight4.mps.gz')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
