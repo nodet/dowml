656bd81c555f876d9c29a5ad496ae57f4cb8d10c
Xavier Nodet <xavier.nodet@fr.ibm.com>
2021-02-13 16:08:09 +0100

    More tests about 'details'

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 import pprint
 from unittest import TestCase, main
-from unittest.mock import Mock, ANY
+from unittest.mock import Mock, ANY, call
 
 from cli import DOWMLInteractive, CommandNeedsJobID
 from dowmlclient import DOWMLClient
@@ -41,17 +41,35 @@ class TestDetails(TestCase):
 
     def setUp(self) -> None:
         self.client = DOWMLInteractive('test_credentials.txt')
-        self.jobs = ['a']
-        self.client.last_job_id = 1
+        self.client.jobs = ['a']
+        self.client.last_job_id = 'a'
 
-    def test_without_full(self):
+    def test_with_no_content(self):
         mock_print = Mock(spec=pprint.pprint)
         mock_client = Mock(spec=DOWMLClient)
-        EXPECTED = {'id': 'the_id'}
-        mock_client.get_job_details.return_value = EXPECTED
+        expected = {'id': 'the_id'}
+        mock_client.get_job_details.return_value = expected
         self.client.client = mock_client
         self.client.do_details('1', printer=mock_print)
-        mock_print.assert_called_once_with(EXPECTED, indent=ANY, width=ANY)
+        mock_print.assert_called_once_with(expected, indent=ANY, width=ANY)
+        mock_client.get_job_details.assert_called_once_with('a', with_contents=False)
+
+    def with_content_helper(self, input):
+        mock_print = Mock(spec=pprint.pprint)
+        mock_client = Mock(spec=DOWMLClient)
+        self.client.client = mock_client
+        self.client.do_details(input, printer=mock_print)
+        mock_client.get_job_details.assert_called_once_with('a', with_contents=True)
+
+    def test_with_content_removed(self):
+        mock_print = Mock(spec=pprint.pprint)
+        mock_client = Mock(spec=DOWMLClient)
+        self.client.client = mock_client
+        self.with_content_helper('1 full')
+        self.with_content_helper('full')
+        self.with_content_helper('full 1')
+        self.with_content_helper('full a')
+        self.with_content_helper('a full')
 
 
 if __name__ == '__main__':
