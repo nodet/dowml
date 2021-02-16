0f8ba08e17164947c612d33d306edfdba360bae3
Xavier Nodet <xavier.nodet@fr.ibm.com>
2021-02-16 13:49:09 +0100

    Rename Client->Lib in tests as well

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -11,24 +11,24 @@ from unittest import TestCase, main
 class TestSolve(TestCase):
 
     def setUp(self) -> None:
-        client = DOWMLLib('test_credentials.txt')
-        client._logger = Mock(spec=Logger)
-        client._client = Mock(spec=APIClient)
-        client._client.deployments = Mock(spec=Deployments)
+        lib = DOWMLLib('test_credentials.txt')
+        lib._logger = Mock(spec=Logger)
+        lib._client = Mock(spec=APIClient)
+        lib._client.deployments = Mock(spec=Deployments)
         domn = Mock()
         domn.SOLVE_PARAMETERS = 'solve-parameters'
         domn.INPUT_DATA = 'input-data'
         domn.OUTPUT_DATA = 'output-data'
-        client._client.deployments.DecisionOptimizationMetaNames = domn
-        client.get_file_as_data = lambda path: 'base-64-content'
-        client._space_id = 'space-id'
-        client._get_deployment_id = Mock(spec=DOWMLLib._get_deployment_id)
-        client._get_deployment_id.return_value = 'deployment-id'
-        self.client = client
+        lib._client.deployments.DecisionOptimizationMetaNames = domn
+        lib.get_file_as_data = lambda path: 'base-64-content'
+        lib._space_id = 'space-id'
+        lib._get_deployment_id = Mock(spec=DOWMLLib._get_deployment_id)
+        lib._get_deployment_id.return_value = 'deployment-id'
+        self.lib = lib
 
     def test_solve_single_file(self):
-        self.client.solve('afiro.mps')
-        create_job_mock = self.client._client.deployments.create_job
+        self.lib.solve('afiro.mps')
+        create_job_mock = self.lib._client.deployments.create_job
         create_job_mock.assert_called_once()
         kall = create_job_mock.call_args
         self.assertEqual(kall.kwargs, {})
@@ -39,30 +39,30 @@ class TestSolve(TestCase):
         self.assertEqual(i['id'], 'afiro.mps')
 
     def test_solve_multiple_files(self):
-        self.client.solve('f1.lp f2.prm')
-        create_job_mock = self.client._client.deployments.create_job
+        self.lib.solve('f1.lp f2.prm')
+        create_job_mock = self.lib._client.deployments.create_job
         create_job_mock.assert_called_once()
         kall = create_job_mock.call_args
         self.assertEqual(kall.args[1]['input-data'][0]['id'], 'f1.lp')
         self.assertEqual(kall.args[1]['input-data'][1]['id'], 'f2.prm')
 
     def test_solve_with_path(self):
-        self.client.solve('/this/is/an/absolute/path')
-        create_job_mock = self.client._client.deployments.create_job
+        self.lib.solve('/this/is/an/absolute/path')
+        create_job_mock = self.lib._client.deployments.create_job
         create_job_mock.assert_called_once()
         kall = create_job_mock.call_args
         self.assertEqual(kall.args[1]['input-data'][0]['id'], 'path')
 
     def test_solve_relative_path(self):
-        self.client.solve('this/is/a/relative/path')
-        create_job_mock = self.client._client.deployments.create_job
+        self.lib.solve('this/is/a/relative/path')
+        create_job_mock = self.lib._client.deployments.create_job
         create_job_mock.assert_called_once()
         kall = create_job_mock.call_args
         self.assertEqual(kall.args[1]['input-data'][0]['id'], 'path')
 
     def test_solve_same_names(self):
         with self.assertRaises(SimilarNamesInJob):
-            self.client.solve('path/f1 another/path/f1')
+            self.lib.solve('path/f1 another/path/f1')
 
 
 if __name__ == '__main__':
