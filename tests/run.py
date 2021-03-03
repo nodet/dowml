567f00c03825bf576863b947f1dbb37661b8d8fc
Xavier Nodet <xavier.nodet@fr.ibm.com>
2021-03-03 14:22:57 +0100

    Code in run.py accesses COS
    The code in run.py, along with the COS credentials stored in
    ~/.bluemix/cos_credentials, can get the list of files in the
    COS bucket.
    
    But this doesn't work in WML:
    
    [2021-03-03T13:19:29Z, SEVERE] ModuleNotFoundError : No module named 'ibm_boto3'
    Traceback (most recent call last):
      File "/var/job_processor/work/wml-dep-bd-do12.10-rqfh32i8-6894c847b5-pmb65/tmp/python/job4320653943208247823.tmp/run.py", line 1, in <module>
        import ibm_boto3

--- tests/run.py
+++ tests/run.py
@@ -1,82 +1,30 @@
-import logging
-import pprint
-import types
+import ibm_boto3
+from ibm_botocore.config import Config
+from ibm_botocore.exceptions import ClientError
 
-import requests
-
-from dowmllib import DOWMLLib
-
-orig_requests_session_send = requests.Session.send
+COS_ENDPOINT = "https://s3.par01.cloud-object-storage.appdomain.cloud"
+COS_BUCKET = 'dowml-client-bucket'
 
 
-def mocked_requests_session_send(*arguments, **kwargs):
-    session, prepared_request = arguments
-    method = prepared_request.method
-    url = prepared_request.url
-    #       2021-02-17 16:59:39,710
-    print(f'           {method} {url}')
-    resp = orig_requests_session_send(*arguments, **kwargs)
-    # print(f'                        {resp.status_code}')
-    return resp
+def test_cos_connection():
+    ibm_boto3.turn_debug_on()
 
-
-old_params = None
-
-
-def patch_params():
-    global old_params
-    result = old_params()
-    # Beware: the parameter list must not have spaces!
-    result['include'] = 'solve_parameters,solve_state,status'
-    return result
+    cos_client = ibm_boto3.resource("s3",
+                                    config=Config(signature_version="oauth"),
+                                    endpoint_url=COS_ENDPOINT,
+                                    )
+    files = cos_client.Bucket(COS_BUCKET).objects.all()
+    try:
+        for file in files:
+            print(f'Item: {file.key} ({file.size} bytes).')
+    except ClientError as be:
+        print(f'CLIENT ERROR: {be}')
+    except Exception as e:
+        print(f'Unable to retrieve contents: {e}')
 
 
 def main():
-    global old_params
-    logging.basicConfig(force=True, format='%(asctime)s %(message)s',
-                        level=logging.DEBUG)
-    requests.Session.send = mocked_requests_session_send
-    lib = DOWMLLib()
-    lib._get_or_make_client()
-
-    old_params = lib._client._params
-    try:
-        lib._client._params = patch_params
-        details = lib.get_job_details('4f90302a-930d-4b97-b98a-d5db8c83b004')
-    finally:
-        lib._client._params = old_params
-
-    pprint.pprint(details)
-
-#
-# class Lib:
-#     def foo(self):
-#         return 'foo'
-#
-#     def print(self):
-#         f = self.foo()
-#         print(f)
-#
-#
-# old_foo = None
-# bar = 'bar'
-#
-#
-# def new_foo(self):
-#     global old_foo
-#     global bar
-#     f = old_foo()
-#     return f + bar
-#
-#
-# def main():
-#     global old_foo
-#     func_type = types.MethodType
-#
-#     my_lib = Lib()
-#     old_foo = my_lib.foo
-#     my_lib.foo = func_type(new_foo, my_lib)
-#     my_lib.print()
+    test_cos_connection()
 
 
 if __name__ == '__main__':
