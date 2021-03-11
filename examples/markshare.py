2f900c14428595753fb81d72ed5344bb71785db9
Xavier Nodet <xavier.nodet@fr.ibm.com>
2021-03-11 13:43:42 +0100

    Comment out parameters from the docplex example

--- examples/markshare.py
+++ examples/markshare.py
@@ -4,12 +4,13 @@ from docplex.mp.model_reader import ModelReader
 def run_model(path):
     print(f'Reading {path}...')
     m = ModelReader.read(path)
-    # Use compressed file on disk to store nodes
-    m.parameters.mip.strategy.file.set(3)
-    m.parameters.timelimit(10)
-    # Limit tree memory to 1 MB
-    m.parameters.workmem(1)
+    # # Use compressed file on disk to store nodes
+    # m.parameters.mip.strategy.file.set(3)
+    # m.parameters.timelimit(10)
+    # # Limit tree memory to 1 MB
+    # m.parameters.workmem(1)
     m.solve(log_output=True)
+
 
 if __name__ == '__main__':
     run_model('markshare1.mps.gz')
