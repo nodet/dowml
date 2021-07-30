5d2662bf1185d7de606ec67e859117b137819135
nodet <xavier.nodet@gmail.com>
2021-07-30 16:35:10 +0200

    [#8] Factor out single solve, add another test

--- tests/test_full.py
+++ tests/test_full.py
@@ -14,16 +14,25 @@ class TestRunningOnWML(TestCase):
         lib = DOWMLLib()
         self.lib = lib
 
-    def test_simple_cplex_inline(self):
+    def job_solves_to_completion(self, paths, type=None, inline=None):
         l = self.lib
-        l.inline = True
-        id = l.solve('examples/afiro.mps')
+        if inline is not None:
+            l.inline = inline
+        if type is not None:
+            l.model_type = type
+        id = l.solve(paths)
         l.wait_for_job_end(id)
         details = l.get_job_details(id)
         self.assertEqual(details['entity']['decision_optimization']['status']['state'], 'completed')
         l.delete_job(id, hard=True)
         self.assertNotIn(id, [j.id for j in l.get_jobs()])
 
+    def test_simple_cplex_inline(self):
+        self.job_solves_to_completion(inline=True, paths='examples/afiro.mps')
+
+    def test_docplex_inline(self):
+        # 'type docplex' 'solve examples/markshare.py examples/markshare1.mps.gz' wait jobs output 'details full' delete
+        self.job_solves_to_completion(inline=True, type='docplex', paths='examples/markshare.py examples/markshare1.mps.gz')
 
 if __name__ == '__main__':
     main()
