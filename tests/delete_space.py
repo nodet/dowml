243ff6a8ddf38253153fa892cac90850f8bdac3d
nodet <xavier.nodet@gmail.com>
2021-08-04 10:11:13 +0200

    [#10] Wait until the space is really gone
    It seems that waiting 5 seconds may not be enough. Let's
    have the deleting script wait until it can't find the space
    in the list any more.

--- tests/delete_space.py
+++ tests/delete_space.py
@@ -1,40 +1,62 @@
+import logging
 import pprint
 import sys
+import time
 
 from ibm_watson_machine_learning import APIClient
 
 from dowmllib import _CredentialsProvider
 
+logger = None
+
+
+def find_space_in_list(client, space_name):
+    global logger
+    spaces = client.spaces.get_details()
+    logger.debug(f'Retrieved spaces.')
+    resources = spaces['resources']
+    logger.debug(f'There are {len(resources)} spaces.')
+    for s in resources:
+        if s['entity']['name'] == space_name:
+            logger.debug(f'Found one with name \'{space_name}\'')
+            space_id = client.spaces.get_id(s)
+            logger.debug(f'Its id is {space_id}')
+            return space_id
+    return None
+
 
 def main():
+    global logger
+    logging.basicConfig(force=True, format='%(asctime)s %(message)s', level=logging.DEBUG)
+    logger = logging.getLogger('delete_space')
+
     # Credentials are provided by the environment
     wml_credentials = _CredentialsProvider().credentials
 
     if _CredentialsProvider.SPACE_NAME not in wml_credentials:
-        print(f'Refusing to destroy the default space')
+        logger.error(f'Refusing to destroy the default space')
         sys.exit(1)
 
     space_name = wml_credentials[_CredentialsProvider.SPACE_NAME]
     client = APIClient(wml_credentials)
-    print(f'Created client.')
-    spaces = client.spaces.get_details()
-    print(f'Retrieved spaces.')
-    resources = spaces['resources']
-    print(f'There are {len(resources)} spaces.')
-    space_id = None
-    for s in resources:
-        if s['entity']['name'] == space_name:
-            print(f'Found one with name \'{space_name}\'')
-            space_id = client.spaces.get_id(s)
-            print(f'Its id is {space_id}')
+    logger.debug(f'Created client.')
+    space_id = find_space_in_list(client, space_name)
     if not space_id:
-        print(f'No space found named \'{space_name}\'')
+        logger.error(f'No space found named \'{space_name}\'')
         sys.exit(1)
 
-    print(f'Deleting space...')
+    logger.debug(f'Deleting space...')
     client.spaces.delete(space_id)
-    print(f'Done.')
+    logger.debug(f'Done.')
 
+    counter = 0
+    while space_id:
+        time.sleep(2)
+        space_id = find_space_in_list(client, space_name)
+        counter = counter + 1
+        if counter > 50:
+            logger.error(f'Waited too long... Exiting.')
+            sys.exit(1)
 
 
 if __name__ == '__main__':
